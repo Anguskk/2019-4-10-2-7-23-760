63d108f78e41e5ca03c5403650964d976336cefa
AngusKK <604202346@qq.com>
2019-05-24 23:17:55 +0800

    finish task

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var dis=inputs.distance;
+    var parkTime=inputs.parkTime;
+    var extra= 0.25*parkTime;
+    var price;
+    if (dis <=2 ) {
+      price = 6 ;
+    } else if(dis <= 8 ) {
+      price = 6 + (dis-2)*0.8;
+    }else {
+      price = 6 + 0.8*6 + (dis -8)*1.2 ;
+    }
+    price += extra;
+    return Math.round(price);
+    //return "price";
 };
